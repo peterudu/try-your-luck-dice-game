0132a3248cdb6dfbef37c19e3d48b085c58d56e3
peterudu <puduevborho7@gmail.com>
2022-12-18 22:58:31 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function rollDice() {
+    let rolledDiceValue = Math.floor(Math.random() * 6) + 1;
+    return rolledDiceValue
+};
+let result = rollDice();
+console.log(result);
+
+function displayResult() {
+
+}
+
+function showDiceImage() {
+
+}
+
+function incrementScore() {
+
+}
+
+function runGame() {
+    
+}
