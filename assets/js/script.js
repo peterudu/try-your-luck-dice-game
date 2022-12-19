cbe94e86bcd44f24c7046838c9356f22aeec5518
peterudu <puduevborho7@gmail.com>
2022-12-19 09:43:16 +0000

    Assign DOM elements selectors and create  rollDice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,61 @@
+const displayArea = document.getElementById("display-result")
+let displayContent = displayArea.innerHTML;
+
+const playerArea  = document.getElementById("player-area")
+const playerImage = document.getElementById("pers-img")
+
+const computerArea = document.getElementById("computer-area")
+const computerImage = document.getElementById("computer-area")
+
+const gameButton = document.getElementById("roll-dice");
+gameButton.addEventListener("click", function (){
+    rollDice();
+});
+
+
+
+// first finish loading the DOM before running the game
+// document.addEventListener("DOMContentLoaded", rollDice) {
+//     let startButton = document.getElementsByTagName("button");
+// }
+
+// function rollDice(number) {
+//     let rolledDiceValue = Math.floor(Math.random() * number) + 1;
+//     return rolledDiceValue
+// };
+
 function rollDice() {
-    let rolledDiceValue = Math.floor(Math.random() * 6) + 1;
-    return rolledDiceValue
+    // create dice values for player and computer
+    let playerDice = Math.floor(Math.random() * 6) + 1;
+    let computerDice = Math.floor(Math.random() * 6) + 1;
+    let diceValues = [playerDice, computerDice];
+    playerDiceValue = diceValues[0];
+    computerDiceValue = diceValues[1]
+    console.log(playerDiceValue)
+    console.log(computerDiceValue)
+    return diceValues
 };
-let result = rollDice();
-console.log(result);
+// rollDice();
+// let diceValues = rollDice();
+// playerDiceValue = diceValues[0]
+// computerDiceValue = diceValues[1]
+// console.log(playerDiceValue);
+// console.log(computerDiceValue);
+// console.log(computerDiceValue);
 
-function displayResult() {
 
-}
+// function displayResult() {
 
-function showDiceImage() {
+// }
 
-}
+// function showDiceImage() {
 
-function incrementScore() {
+// }
 
-}
+// function incrementScore() {
 
-function runGame() {
+// }
+
+// function runGame() {
     
-}
+// }
